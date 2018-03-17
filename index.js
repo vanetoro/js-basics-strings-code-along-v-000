49bd4c669a85cc8de791c8330482d6f7f230d8ef
Vanessa Toro <vanetoro84@gmail.com>
2018-03-17 01:49:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser  = "Vane"
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + ' !'
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + " !"
+
+const shortGreeting = "Welcome, " + currentUser[0].toUpperCase() + "!"
+
